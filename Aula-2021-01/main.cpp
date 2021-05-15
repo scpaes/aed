167f88918ec28324396c9823e6c5e3608401b7b9
Samuel Paes <scp2@cin.ufpe.br>
2021-05-15 18:13:43 -0300

    Alteração no código da aula 1

--- Aula-2021-01/main.cpp
+++ Aula-2021-01/main.cpp
@@ -2,7 +2,26 @@
 
 using namespace std;
 
+class Retangulo
+{
+private:
+    int altura;
+    int largura;
+public:
+    Retangulo(int altura, int largura)
+    {
+        this->altura = altura;
+        this->largura = largura;
+    }
+    int getAltura()
+    {
+        return altura;
+    }
+};
+
 int main()
 {
-    cout << "Hello World" << endl;
+    int a = 10;
+    Retangulo r1 (10, 20);
+    cout << "r1.altura = " << r1.getAltura() << endl;
 }
