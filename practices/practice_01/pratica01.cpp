f667b1bfb7e1b6c481711ff5deb27c6262b70362
scpaes <scp2@cin.ufpe.br>
2023-09-15 21:43:22 -0300

    practice_01 part 2

--- practices/practice_01/pratica01.cpp
+++ practices/practice_01/pratica01.cpp
@@ -2,6 +2,26 @@
 
 using namespace std;
 
+class Veiculo{
+    private:
+        string nome;
+
+    public:
+        Veiculo(const char *param){
+            this->nome = string(param);
+            cout << "Objeto com o nome" << this->nome << "criado" << endl;
+        }
+        ~Veiculo(){
+            cout << "Objeto com o nome" << this->nome << "destruido" << endl;
+        }
+};
+
 int main(){
-    cout << "Primeira aplicação C++" << endl;
+    Veiculo veiculo1("Fusca");
+    {
+        Veiculo veiculo2("Brasilia");
+        {
+            Veiculo veiculo3("Gol");
+        }
+    }
 }
