12344330c16c1fbbc60863aaac2e0bbd7cbf99b8
scpaes <scp2@cin.ufpe.br>
2023-09-21 03:09:17 -0300

    pratica 3 parte 3

--- practices/practice_03/funcoes.h
+++ practices/practice_03/funcoes.h
@@ -5,21 +5,39 @@
 #ifndef PRACTICE_03_FUNCOES_H
 #define PRACTICE_03_FUNCOES_H
 
-void trocar(int &a, int &b) {
-    /* troca valores de a e b entre si */
-    int temp = a;
-    a = b;
-    b = temp;
-}
+#include <cstring>
 
-int maximo(const int a, const int b) {
-    /* retorna maior entre a e b */
-    return (a > b) ? a : b;
-}
+namespace funcoes{
+    template<class T>
+    void trocar(T &a, T &b) {
+        /* troca valores de a e b entre si */
+        int temp = a;
+        a = b;
+        b = temp;
+    }
 
-int minimo(const int a, const int b) {
-    /* retorna menor entre a e b */
-    return (a > b) ? b : a;
+    template<class T>
+    T maximo(T a, T b) {
+        /* retorna maior entre a e b */
+        return (a > b) ? a : b;
+    }
+
+    template<>
+    const char *maximo(const char *a, const char *b) {
+        /* retorna maior entre a e b */
+        return (strcmp(a, b) > 0) ? a : b;
+    }
+
+    template<class T>
+    T minimo(T a, T b) {
+        /* retorna menor entre a e b */
+        return (a > b) ? b : a;
+    }
+    template<>
+    const char *minimo(const char *a, const char *b) {
+        /* retorna menor entre a e b */
+        return (strcmp(a, b) > 0) ? b : a;
+    }
 }
 
 #endif //PRACTICE_03_FUNCOES_H
--- practices/practice_03/pratica3.cpp
+++ practices/practice_03/pratica3.cpp
@@ -3,13 +3,20 @@
 
 
 using namespace std;
+using namespace funcoes;
 
 int main() {
-    int x = 5, y = 10, z = 30;
+    // int x = 5, y = 10, z = 30;
+    float x = 5.5, y = 10.15, z = 30.7;
     cout << "Antes: x = " << x << " y = " << y << endl;
     trocar(x, y);
     cout << "Depois : x = " << x << " y = " << y << endl;
     cout << "Minimo entre " << x << " e " << y << ": " << minimo(x, y) << endl;
     cout << "Maximo entre " << y << " e " << z << ": " << maximo(y, z) << endl;
+
+    // minimo and maximo with strings
+    const char *a = "StringA", *b = "StringB";
+    cout << "Minimo entre strings " << a << " e " << b << ": " << minimo(a, b) << endl;
+    cout << "Maximo entre strings " << a << " e " << b << ": " << maximo(a, b) << endl;
     return 0;
 }
